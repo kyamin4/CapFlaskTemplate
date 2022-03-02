eb241bacf7defe057af47e6f5a2fb0f0c2696510
Jonathan Yim <s_jonathan.yim@ousd.org>
2022-03-02 08:56:29 -0800

    .

--- app/routes/default.py
+++ app/routes/default.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test')
+
+def test():
+    return render_template('test.html')
